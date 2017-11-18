b956264cb6a3646f89aa9ea1d4a81490e4be34f2
GityUpNow <kim-cheng@gmx.de>
2017-11-18 15:13:44 +0100

    Did some changes to the code so that the button shouldn't trigger randomly anymore. Also updated the ip

--- init.lua
+++ init.lua
@@ -1,28 +1,53 @@
 BUTTON = 7
 BUTTON_DEBOUNCE_MS = 5000 -- debounce and rate-limit at once
+NOISE_DEBOUNCE_MS = 30 -- should fix the "random trigger" problem
 
 host = {
-  address = "192.168.23.42";
+  address = "10.214.226.24";
   port = 23421;
 }
 
+sum = 0
+i   = 1
+
 function button_init()
-  gpio.trig(BUTTON, "down", button_callback)
-  gpio.mode(BUTTON, gpio.INT, gpio.PULLUP)
+  gpio.trig(BUTTON, "down", button_lowCallback)
+  gpio.mode(BUTTON, gpio.INT)
 end
 
+function button_lowCallback()
+  tmr.alarm(0, 1, tmr.ALARM_SEMI, button_count)
+  gpio.trig(BUTTON, "none") 
+end
+
+function button_count()
+  if(i > NOISE_DEBOUNCE_MS) then
+    i = 0
+    tmr.unregister(0)
+    if(sum < 5) then
+       sum = 0
+       rimshot()
+    else
+       sum = 0
+       print("Too noisy!")
+       button_init()
+    end
+  else
+    i = i + 1
+    sum = sum + gpio.read(BUTTON)
+    tmr.start(0)
+  end
+end
+    
+
 function rimshot ()
+  gpio.trig(BUTTON, "none")
   socket = net.createConnection(net.UDP, 0)
   socket:connect(host.port, host.address)
   socket:send("rimshot\n")
   socket:close()
   print("ba-dumm-tss!")
-end
-
-function button_callback()
-  gpio.mode(BUTTON, gpio.INPUT)
   tmr.alarm(0, BUTTON_DEBOUNCE_MS, tmr.ALARM_SINGLE, button_init)
-  rimshot()
 end
-
+    
 button_init()
